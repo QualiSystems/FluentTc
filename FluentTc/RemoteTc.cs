a67ba21e921a2ea76d91d44ad9d31218248e1399
igoro1975@gmail.com <igoro1975@gmail.com>
2015-10-18 14:46:52 +0300

    Refactoring - 1

--- FluentTc/RemoteTc.cs
+++ FluentTc/RemoteTc.cs
@@ -14,6 +14,11 @@ namespace FluentTc
             return this;
         }
 
+        public List<TcAgent> GetAgents(Action<TcAgentHavingBuilder> having)
+        {
+            return new List<TcAgent>();
+        }
+
         public List<Build> GetBuilds(Action<BuildHavingBuilder> having, Action<CountBuilder> count,
             Action<BuildPropertyBuilder> include)
         {
@@ -292,5 +297,33 @@ namespace FluentTc
 
     public class Build
     {
+    }
+
+
+    public class TcAgent
+    {
+
+    }
+
+    public class TcAgentHavingBuilder
+    {
+        private readonly List<string> m_Having = new List<string>();
+
+        public TcAgentHavingBuilder Authorized()
+        {
+            m_Having.Add("authorized:" + bool.TrueString);
+            return this;
+        }
+
+        public TcAgentHavingBuilder Connected()
+        {
+            m_Having.Add("connected:" + bool.TrueString);
+            return this;
+        }
+        public TcAgentHavingBuilder Enabled()
+        {
+            m_Having.Add("enabled:" + bool.TrueString);
+            return this;
+        }
     }
 }
