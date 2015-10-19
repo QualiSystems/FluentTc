a7eec35d2e11ad6ea0b05bf915c18b3546c1f650
Boris Modylevsky <borismod@gmail.com>
2015-10-19 09:49:04 +0300

    RemoteTc was refactored to make sure Connect is called only once and no other method is not called before Connect

--- FluentTc/RemoteTc.cs
+++ FluentTc/RemoteTc.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using FluentTc.Domain;
 using FluentTc.Locators;
 
 namespace FluentTc
@@ -12,103 +9,14 @@ namespace FluentTc
         private IBuildsRetriever m_BuildsRetriever;
         private IAgentsRetriever m_AgentsRetriever;
 
-        public RemoteTc Connect(Action<TeamCityConfigurationBuilder> connect)
+        public IConnectedTc Connect(Action<TeamCityConfigurationBuilder> connect)
         {
             var teamCityConfigurationBuilder = new TeamCityConfigurationBuilder();
             connect(teamCityConfigurationBuilder);
             m_Caller = new TeamCityCaller(teamCityConfigurationBuilder.GetITeamCityConnectionDetails());
             m_BuildsRetriever = new BuildsRetriever(m_Caller, new BuildHavingBuilderFactory(new BuildConfigurationHavingBuilderFactory(new BuildProjectHavingBuilderFactory()) ));
             m_AgentsRetriever = new AgentsRetriever(m_Caller, new AgentHavingBuilderFactory());
-            return this;
-        }
-
-        public List<Build> GetBuilds(Action<IBuildHavingBuilder> having, Action<CountBuilder> count,
-            Action<BuildIncludeBuilder> include)
-        {
-            return m_BuildsRetriever.GetBuilds(having, count, include);
-        }
-
-        public List<Agent> GetAgents(Action<IAgentHavingBuilder> having)
-        {
-            return m_AgentsRetriever.GetAgents(having);
-        }
-
-        public List<Build> GetBuilds(Action<IBuildHavingBuilder> having, Action<BuildIncludeBuilder> include)
-        {
-            return m_BuildsRetriever.GetBuilds(having, _ => _.All(), include);
-        }
-
-        public List<Build> GetBuilds(Action<IBuildHavingBuilder> having)
-        {
-            return m_BuildsRetriever.GetBuilds(having, _ => _.All(), _ => _.IncludeDefaults());
-        }
-
-        public Build GetBuild(Action<IBuildHavingBuilder> having, Action<BuildIncludeBuilder> include)
-        {
-            var builds = GetBuilds(having, include);
-            if (!builds.Any() ) throw new BuildNotFoundException();
-            if (builds.Count() > 1 ) throw new MoreThanOneBuildFoundException();
-            return builds.Single();
-        }
-
-        public Build GetBuild(Action<IBuildHavingBuilder> having)
-        {
-            return GetBuild(having, _ => _.IncludeDefaults());
-        }
-
-        public BuildConfiguration GetBuildConfiguration(Action<BuildConfigurationHavingBuilder> having, Action<BuildConfigurationPropertyBuilder> include)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IList<BuildConfiguration> GetBuildConfigurations(Action<BuildConfigurationHavingBuilder> having, Action<BuildConfigurationPropertyBuilder> include)
-        {
-            throw new NotImplementedException();
-        }
-
-        public BuildConfiguration SetParameters(Action<BuildConfigurationHavingBuilder> having, Action<BuildParameterValueBuilder> parameters)
-        {
-            throw new NotImplementedException();
-        }
-
-        public BuildConfiguration RunBuildConfiguration(Action<BuildConfigurationHavingBuilder> having, Action<BuildParameterValueBuilder> parameters)
-        {
-            throw new NotImplementedException();
-        }
-
-        public BuildConfiguration RunBuildConfiguration(Action<BuildConfigurationHavingBuilder> having, Action<AgentLocatorBuilder> onAgent)
-        {
-            throw new NotImplementedException();
-        }
-
-        public BuildConfiguration RunBuildConfiguration(Action<BuildConfigurationHavingBuilder> having, Action<AgentLocatorBuilder> onAgent, Action<BuildParameterValueBuilder> parameters)
-        {
-            throw new NotImplementedException();
-        }
-
-        public BuildConfiguration RunBuildConfiguration(Action<BuildConfigurationHavingBuilder> having)
-        {
-            throw new NotImplementedException();
-        }
-        
-        public IList<BuildConfiguration> GetBuildConfigurations(Action<BuildProjectHavingBuilder> having, Action<BuildConfigurationPropertyBuilder> include)
-        {
-            throw new NotImplementedException();
-        }
-
-        public BuildConfiguration CreateBuildConfiguration(Action<BuildProjectHavingBuilder> having, string buildConfigurationName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void AttachBuildConfigurationToTemplate(Action<BuildConfigurationHavingBuilder> having, Action<BuildTemplateHavingBuilder> templateHaving)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteBuildConfiguration(Action<BuildConfigurationHavingBuilder> having)
-        {
-            throw new NotImplementedException();
+            return new ConnectedTc(m_Caller, m_BuildsRetriever, m_AgentsRetriever);
         }
     }
 }
