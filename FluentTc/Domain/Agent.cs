7150b3fdf0a60d5ab008edf79c0f17393deb5a7a
igoro1975 <igoro1975@gmail.com>
2015-10-19 10:39:19 +0300

    Agent class - properties added.

--- FluentTc/Domain/Agent.cs
+++ FluentTc/Domain/Agent.cs
@@ -2,6 +2,9 @@ namespace FluentTc.Domain
 {
     public class Agent
     {
-        
+        public int Id { get; set; }
+        public int TypeId { get; set; }
+        public string Name { get; set; }
+        public string Href { get; set; }
     }
 }
