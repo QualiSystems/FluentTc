b986dc03b239d6771f989515bb21285c4aa62883
igoro1975 <igoro1975@gmail.com>
2015-10-19 09:59:42 +0300

    Agents tests added

---
+++ FluentTc.Tests/Locators/AgentHavingBuilderTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using FluentTc.Locators;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace FluentTc.Tests.Locators
+{
+    [TestFixture]
+    public class AgentHavingBuilderTests
+    {
+        [Test]
+        public void Connected()
+        {
+            // Arrange
+            var fixture = Auto.Fixture();
+            var agentHavingBuilder = fixture.Create<AgentHavingBuilder>();
+
+            // Act
+            var havingBuilder = agentHavingBuilder.OnlyConnected();
+
+            // Assert
+            ((IAgentHavingBuilder)havingBuilder).GetLocator().Should().Be("connected:True");
+        }
+
+        [Test]
+        public void Authorized()
+        {
+            // Arrange
+            var fixture = Auto.Fixture();
+            var agentHavingBuilder = fixture.Create<AgentHavingBuilder>();
+
+            // Act
+            var havingBuilder = agentHavingBuilder.OnlyAuthorized();
+
+            // Assert
+            ((IAgentHavingBuilder)havingBuilder).GetLocator().Should().Be("authorized:True");
+        }
+    }
+}
