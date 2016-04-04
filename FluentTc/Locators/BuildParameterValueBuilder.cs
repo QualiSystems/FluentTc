e81882fcfa4c96b107fba3ded74ba91467c3fc83
sokolov.s <sokolov.s@mirkvartir.ru>
2016-04-04 20:55:25 +0300

    backward compatibility fix

--- FluentTc/Locators/BuildParameterValueBuilder.cs
+++ FluentTc/Locators/BuildParameterValueBuilder.cs
@@ -5,12 +5,19 @@ namespace FluentTc.Locators
 {
     public interface IBuildParameterValueBuilder
     {
+        IBuildParameterValueBuilder Parameter(string name, string value);
         IBuildParameterValueBuilder Parameter(string name, string value, string rawValue);
     }
 
     public class BuildParameterValueBuilder : IBuildParameterValueBuilder
     {
         readonly List<Property> m_Properties = new List<Property>();
+
+        public IBuildParameterValueBuilder Parameter(string name, string value)
+        {
+            m_Properties.Add(new Property { Name = name, Value = value, Type = null });
+            return this;
+        }
 
         public IBuildParameterValueBuilder Parameter(string name, string value, string rawValue)
         {
