46e54addc0c364198d4a26167b1fdff66f6921c9
nex-54 <nex-54@skbkontur.ru>
2016-07-13 21:21:07 +0500

    Add test for Properties

--- FluentTc.Tests/Engine/BuildModelToBuildConverterTests.cs
+++ FluentTc.Tests/Engine/BuildModelToBuildConverterTests.cs
@@ -93,5 +93,28 @@ namespace FluentTc.Tests.Engine
             builds.Single().Status.HasValue.Should().BeFalse();
             builds.Single().BuildConfiguration.Id.Should().Be("bt2");
         }
+
+        [Test]
+        public void ConvertToBuilds_BuildProperties()
+        {
+            var buildModelToBuildConverter = new BuildModelToBuildConverter();
+            var buildWrapper = new BuildWrapper
+            {
+                Build = new List<BuildModel> {new BuildModel
+                {
+                    Status = "SUCCESS",
+                    Properties = new Properties
+                    {
+                        Property = new List<Property> { new Property { Name = "Property1", Value = "Value1"} }
+                    }
+                }},
+                Count = "1"
+            };
+            var builds = buildModelToBuildConverter.ConvertToBuilds(buildWrapper);
+
+            // Assert
+            builds.Single().Properties.Property.Single().Name.Should().Be("Property1");
+            builds.Single().Properties.Property.Single().Value.Should().Be("Value1");
+        }
     }
 }
