0541be06436cc53efb2c6dc319dda7bde56eb4b5
nex-54 <nex-54@skbkontur.ru>
2016-07-14 10:49:59 +0500

    try fix percentage of statement coverage

--- FluentTc.Tests/AcceptanceTests.cs
+++ FluentTc.Tests/AcceptanceTests.cs
@@ -553,6 +553,33 @@ namespace FluentTc.Tests
         }
 
         [Test]
+        public void RunBuildConfiguration_BuildResponse_WithParameters()
+        {
+            // Arrange
+            Action<IBuildConfigurationHavingBuilder> having = _ => _.Id("FluentTc");
+            var teamCityCaller = CreateTeamCityCaller();
+            var buildConfigurationRetriever = A.Fake<IBuildConfigurationRetriever>();
+
+            A.CallTo(() => buildConfigurationRetriever.GetSingleBuildConfiguration(having))
+                .Returns(new BuildConfiguration { Id = "bt2" });
+            A.CallTo(() =>
+                teamCityCaller.PostFormat<BuildModel>(
+                    "<build>\r\n<buildType id=\"bt2\"/>\r\n<properties>\r\n<property name=\"param1\" value=\"value1\"/>\r\n</properties>\r\n</build>\r\n",
+                    HttpContentTypes.ApplicationXml, HttpContentTypes.ApplicationJson, "/app/rest/buildQueue", A<object[]>.Ignored))
+                .Returns(new BuildModel {Id = 123, Status = "SUCCESS"});
+
+            var connectedTc = new RemoteTc().Connect(_ => _.AsGuest(), teamCityCaller, buildConfigurationRetriever);
+
+            // Act
+            var build = connectedTc.RunBuildConfiguration(having, p => p.Parameter("param1", "value1"));
+
+            // Assert
+            build.Should().NotBe(null);
+            build.Id.ShouldBeEquivalentTo(123);
+            build.Status.ShouldBeEquivalentTo(BuildStatus.Success);
+        }
+
+        [Test]
         public void GetBuildConfigurations_ByName()
         {
             // Arrange
