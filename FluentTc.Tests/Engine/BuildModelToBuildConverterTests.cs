6c597940260f46b15b3f877fc888ceacd4be1caf
nex-54 <nex-54@skbkontur.ru>
2016-07-15 17:23:53 +0500

    tests to Build.State

--- FluentTc.Tests/Engine/BuildModelToBuildConverterTests.cs
+++ FluentTc.Tests/Engine/BuildModelToBuildConverterTests.cs
@@ -110,5 +110,39 @@ namespace FluentTc.Tests.Engine
             build.Status.Should().Be(BuildStatus.Failure);
             build.BuildConfiguration.Id.Should().Be("bt2");
         }
+
+        [Test]
+        public void ConvertToBuilds_State()
+        {
+            var buildModelToBuildConverter = new BuildModelToBuildConverter();
+            var buildWrapper = new BuildWrapper
+            {
+                Build = new List<BuildModel>
+                {
+                    new BuildModel
+                    {
+                        Id = 1,
+                        State = "Queued"
+                    },
+                    new BuildModel
+                    {
+                        Id = 2,
+                        State = "running"
+                    },
+                    new BuildModel
+                    {
+                        Id = 3,
+                        State = "FINISHED"
+                    }
+                },
+                Count = "3"
+            };
+            var builds = buildModelToBuildConverter.ConvertToBuilds(buildWrapper);
+
+            // Assert
+            builds.Single(_ => _.Id == 1).State.Should().Be(BuildState.Queued);
+            builds.Single(_ => _.Id == 2).State.Should().Be(BuildState.Running);
+            builds.Single(_ => _.Id == 3).State.Should().Be(BuildState.Finished);
+        }
     }
 }
