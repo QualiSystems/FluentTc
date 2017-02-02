ee3957709f57f177d825e59010cea5c9714a5429
Daniel Dreibrodt <daniel.dreibrodt@gmail.com>
2017-02-02 10:07:13 +0100

    Add SourceBuildType property to ArtifactDependency

--- FluentTc/Domain/ArtifactDependency.cs
+++ FluentTc/Domain/ArtifactDependency.cs
@@ -1,5 +1,7 @@
 namespace FluentTc.Domain
 {
+    using JsonFx.Json;
+
     public class ArtifactDependency
     {
         public override string ToString()
@@ -8,7 +10,12 @@ namespace FluentTc.Domain
         }
 
         public string Id { get; set; }
+
         public string Type { get; set; }
+
         public Properties Properties { get; set; }
+
+        [JsonName("source-buildType")]
+        public SourceBuildType SourceBuildType { get; set; }
     }
 }
