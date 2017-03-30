8c12f584adde1f857585a92a4b2c37b2f1083554
Gilberto Polzoni <g.polzoni@loccioni.com>
2017-03-30 17:13:16 +0200

    Refs #93. Now it's possible to modify description of build project or build configuration.
    According to:
    - https://confluence.jetbrains.com/display/TW/REST+API#RESTAPI-ProjectSettings
    - https://confluence.jetbrains.com/display/TW/REST+API#RESTAPI-BuildConfigurationAndTemplateSettings

---
+++ FluentTc/Locators/BuildFieldValueBuilder.cs
@@ -0,0 +1,45 @@
+﻿using FluentTc.Domain;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace FluentTc.Locators
+{
+    public interface IBuildFieldValueBuilder
+    {
+        IBuildFieldValueBuilder Name(string value);
+        IBuildFieldValueBuilder Description(string value);
+        IBuildFieldValueBuilder Paused(bool value);
+    }
+
+    public class BuildFieldValueBuilder: IBuildFieldValueBuilder
+    {
+        readonly List<Property> m_Properties = new List<Property>();
+
+        public IBuildFieldValueBuilder Description(string value)
+        {
+            m_Properties.Add(new Property { Name = nameof(Description).ToLower(), Value = value, Type = null });
+            return this;
+        }
+
+        public IBuildFieldValueBuilder Paused(bool value)
+        {
+            m_Properties.Add(new Property
+                {
+                Name = nameof(Name).ToLower(),
+                Value = value.ToString(CultureInfo.InvariantCulture).ToLowerInvariant(),
+                Type = null });
+            return this;
+        }
+
+        public IBuildFieldValueBuilder Name(string value)
+        {
+            m_Properties.Add(new Property { Name = nameof(Name).ToLower(), Value = value, Type = null });
+            return this;
+        }
+
+        public List<Property> GetFields()
+        {
+            return m_Properties;
+        }
+    }
+}
---
+++ FluentTc/Locators/BuildProjectFieldValueBuilder.cs
@@ -0,0 +1,45 @@
+﻿using FluentTc.Domain;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace FluentTc.Locators
+{
+    public interface IBuildProjectFieldValueBuilder
+    {
+        IBuildProjectFieldValueBuilder Name(string value);
+        IBuildProjectFieldValueBuilder Description(string value);
+        IBuildProjectFieldValueBuilder Archived(bool value);
+    }
+
+    public class BuildProjectFieldValueBuilder: IBuildProjectFieldValueBuilder
+    {
+        readonly List<Property> m_Properties = new List<Property>();
+
+        public IBuildProjectFieldValueBuilder Description(string value)
+        {
+            m_Properties.Add(new Property { Name = nameof(Description).ToLower(), Value = value, Type = null });
+            return this;
+        }
+
+        public IBuildProjectFieldValueBuilder Archived(bool value)
+        {
+            m_Properties.Add(new Property
+                {
+                Name = nameof(Name).ToLower(),
+                Value = value.ToString(CultureInfo.InvariantCulture).ToLowerInvariant(),
+                Type = null });
+            return this;
+        }
+
+        public IBuildProjectFieldValueBuilder Name(string value)
+        {
+            m_Properties.Add(new Property { Name = nameof(Name).ToLower(), Value = value, Type = null });
+            return this;
+        }
+
+        public List<Property> GetFields()
+        {
+            return m_Properties;
+        }
+    }
+}
